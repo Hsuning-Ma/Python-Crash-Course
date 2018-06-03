f6e1e7b2f7cdd5fbef9cf2978780b6722d55938b
Hsuning Ma <manginax.ma@gmail.com>
2018-06-04 01:02:45 +0800

    changing the label type and graph thickness

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,4 +1,10 @@
 import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+plt.plot(squares, linewidth = 5)
+#   Set chart title and label axis
+plt.title("Square Numbers", fontsize=24)
+plt.ylabel("square of value", fontsize=14)
+plt.xlabel("value", fontsize=14)
+#   Set size of tick labels
+plt.tick_params(axis="both", labelsize=14)
 plt.show()
