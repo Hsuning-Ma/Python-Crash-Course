bcda7f26cd9ca1a01922fb1eef997da81ccf736f
Hsuning Ma <manginax.ma@gmail.com>
2018-06-13 01:07:30 +0800

    rolling dices of different sides

--- RollingDiceWithPygal/die_visual.py
+++ RollingDiceWithPygal/die_visual.py
@@ -2,10 +2,10 @@ from die import Die
 import pygal
 #   Create two Dies with six sides
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 #   Make some rolls, and store the results in a list
 results = []
-for roll_num in range(1000) :
+for roll_num in range(500000) :
   result = die_1.roll() + die_2.roll()
   results.append(result)
 #   Analyze the results
@@ -16,8 +16,10 @@ for value in range(2, max_result + 1) :
   frequencies.append(frequency)
 #   Visualize the results
 hist = pygal.Bar()
-hist.title = "Results of rolling two D6 dices 1000 times"
-hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.title = "Results of rolling a D6 and a D10 for 500000 times"
+hist.x_labels = [
+  '2', '3', '4', '5', '6', '7',
+  '8', '9', '10', '11', '12', '13', '14', '15', '16']
 hist.x_title = "Result"
 hist.y_title = "Frequency of result"
 hist.add("D6 + D6", frequencies)
