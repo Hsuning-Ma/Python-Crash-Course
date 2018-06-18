93c4b6934944f9e607f81e97b69bf66801f1003d
Hsuning Ma <manginax.ma@gmail.com>
2018-06-18 23:21:56 +0800

    plotting a longer timeframe

--- DownloadingData/highs_lows.py
+++ DownloadingData/highs_lows.py
@@ -1,7 +1,8 @@
 import csv
 from matplotlib import pyplot as plt
 from datetime import datetime
-filename = 'sitka_weather_07-2014.csv'
+#   filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f :
   reader = csv.reader(f)
   header_row = next(reader)
@@ -14,12 +15,13 @@ with open(filename) as f :
     dates.append(current_date)
     high = int(row[1])
     highs.append(high)
-  print(highs)
+  #   print(highs)
 #   Plot data
 fig = plt.figure(dpi = 128, figsize = (10, 6))
 plt.plot(dates, highs, c = "red")
 #   Format plot
-plt.title("Daily high temperatures, July 2014", fontsize = 24)
+#   plt.title("Daily high temperatures, July 2014", fontsize = 24)
+plt.title("Daily high temperatures - 2014", fontsize = 24)
 plt.xlabel("", fontsize = 16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize = 16)
