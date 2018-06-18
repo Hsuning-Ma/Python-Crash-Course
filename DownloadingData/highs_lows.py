b0df950ce56fc3e3d248c9fb71b8d9d7e21a3839
Hsuning Ma <manginax.ma@gmail.com>
2018-06-18 23:57:01 +0800

    plotting a second data series

--- DownloadingData/highs_lows.py
+++ DownloadingData/highs_lows.py
@@ -8,20 +8,23 @@ with open(filename) as f :
   header_row = next(reader)
   # for index, column_header in enumerate(header_row) :
   #   print(index, column_header)
-  dates, highs = [], []
+  dates, highs, lows = [], [], []
   for row in reader :
     # highs.append(row[1])
     current_date = datetime.strptime(row[0], "%Y-%m-%d")
     dates.append(current_date)
     high = int(row[1])
     highs.append(high)
+    low = int(row[3])
+    lows.append(low)
   #   print(highs)
 #   Plot data
 fig = plt.figure(dpi = 128, figsize = (10, 6))
 plt.plot(dates, highs, c = "red")
+plt.plot(dates, lows, c = "blue")
 #   Format plot
 #   plt.title("Daily high temperatures, July 2014", fontsize = 24)
-plt.title("Daily high temperatures - 2014", fontsize = 24)
+plt.title("Daily high and low temperatures - 2014", fontsize = 24)
 plt.xlabel("", fontsize = 16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize = 16)
