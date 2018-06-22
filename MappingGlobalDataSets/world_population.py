7359f47e58b23979aa8c5c8475f4821f3d0b0a17
Hsuning Ma <manginax.ma@gmail.com>
2018-06-23 02:47:13 +0800

    obtaining two-digit country codes

--- MappingGlobalDataSets/world_population.py
+++ MappingGlobalDataSets/world_population.py
@@ -1,4 +1,5 @@
 import json
+from countries_codes import get_country_code
 #   Load the data into a list
 filename = "population_data.json"
 with open(filename) as f :
@@ -8,4 +9,9 @@ for pop_dict in pop_data :
   if pop_dict["Year"] =="2010" :
     country_name = pop_dict["Country Name"]
     population = int(float(pop_dict["Value"]))
-    print(country_name + ": " + str(population))
+    code = get_country_code(country_name)
+    if code :
+      print(code + ": " + str(population))
+    else :
+      print("Error - " + country_name)
+    #print(country_name + ": " + str(population))
