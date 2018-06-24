17804ea0d028d9a796707cdf77258facdd0cca42
Hsuning Ma <manginax.ma@gmail.com>
2018-06-24 23:57:12 +0800

    summarizing the top repositories

--- WorkingWithAPIs/python_repos.py
+++ WorkingWithAPIs/python_repos.py
@@ -12,16 +12,23 @@ print("totle repositories: ", response_dict['total_count'])
 print("type of response_dict: ", type(response_dict))
 repo_dicts = response_dict['items']
 print("Repositories returned: ", len(repo_dicts))
-#   Examine the first repository
-repo_dict = repo_dicts[0]
-# print("\nKeys : ", len(repo_dict))
-# for key in sorted(repo_dict.keys()) :
-#   print(key)
-print("\nSelected information about first repository: ")
-print("Name: ", repo_dict['name'])
-print('Owner: ', repo_dict['owner']['login'])
-print("Stars: ", repo_dict['stargazers_count'])
-print("Repository: ", repo_dict['html_url'])
-print("Created: ", repo_dict['created_at'])
-print("Updated: ", repo_dict["updated_at"])
-print("Description: ", repo_dict["description"])
+# #   Examine the first repository
+# repo_dict = repo_dicts[0]
+# # print("\nKeys : ", len(repo_dict))
+# # for key in sorted(repo_dict.keys()) :
+# #   print(key)
+# print("\nSelected information about first repository: ")
+# print("Name: ", repo_dict['name'])
+# print('Owner: ', repo_dict['owner']['login'])
+# print("Stars: ", repo_dict['stargazers_count'])
+# print("Repository: ", repo_dict['html_url'])
+# print("Created: ", repo_dict['created_at'])
+# print("Updated: ", repo_dict["updated_at"])
+# print("Description: ", repo_dict["description"])
+print("\nSelected information about each repository: ")
+for repo_dict in repo_dicts :
+  print("\nName: ", repo_dict["name"])
+  print("Owner: ", repo_dict["owner"]["login"])
+  print("Stars: ", repo_dict["stargazers_count"])
+  print("Repository: ", repo_dict["html_url"])
+  print("Description: ", repo_dict['description'])
