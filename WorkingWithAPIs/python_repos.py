359edd367fe5507a79a83b84d5461cbee90204f0
Hsuning Ma <manginax.ma@gmail.com>
2018-06-24 23:52:16 +0800

    working with the response dictionary

--- WorkingWithAPIs/python_repos.py
+++ WorkingWithAPIs/python_repos.py
@@ -5,5 +5,23 @@ r = requests.get(url)
 print("Status code: ", r.status_code)
 #   Store API response in a variable
 response_dict = r.json()
-#   Process results
-print(response_dict.keys())
+# #   Process results
+# print(response_dict.keys())
+print("totle repositories: ", response_dict['total_count'])
+#   Explore information about the repositories
+print("type of response_dict: ", type(response_dict))
+repo_dicts = response_dict['items']
+print("Repositories returned: ", len(repo_dicts))
+#   Examine the first repository
+repo_dict = repo_dicts[0]
+# print("\nKeys : ", len(repo_dict))
+# for key in sorted(repo_dict.keys()) :
+#   print(key)
+print("\nSelected information about first repository: ")
+print("Name: ", repo_dict['name'])
+print('Owner: ', repo_dict['owner']['login'])
+print("Stars: ", repo_dict['stargazers_count'])
+print("Repository: ", repo_dict['html_url'])
+print("Created: ", repo_dict['created_at'])
+print("Updated: ", repo_dict["updated_at"])
+print("Description: ", repo_dict["description"])
