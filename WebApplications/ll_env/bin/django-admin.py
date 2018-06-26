6f97f82907e8a34a014dc1a3658abfad6ef144dc
Hsuning Ma <manginax.ma@gmail.com>
2018-06-27 01:08:09 +0800

    installing django

---
+++ WebApplications/ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/z/Projects/PythonLearning/PythonCrashCourse/WebApplications/ll_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
