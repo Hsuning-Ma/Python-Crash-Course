0e1750da0830ff6b4f8c7c9c1785d445717a2e0d
Hsuning Ma <manginax.ma@gmail.com>
2018-07-01 01:01:10 +0800

    I don't know

--- WebApplications/learning_logs/urls.py
+++ WebApplications/learning_logs/urls.py
@@ -1,11 +1,11 @@
 """Defines URL patterns for learning_logs"""
 from django.urls import path
 # from . import views as learning_logs_views
-from learning_logs.views import index
+from learning_logs.views import index, topics
 urlpatterns = [
   #   Home page
   # url(r'^$', views.index, name = "index")
-  path(r'', index, name = "index")
+  path(r'', index, name = "index"),
   # path(r'^$', index, name = "index")
   #   Show all topics
   path(r'^topics/$', topics, name = "topics"),
